c8ec30585c35ee89c017d30b8af5dff9d59ca6e7
USER <narajy@gmail.com>
2022-10-10 22:48:21 +0900

    동시성 이슈 해결 연습

---
+++ src/main/java/com/jyp/product/stock/facade/NamedLockStockFacade.java
@@ -0,0 +1,29 @@
+package com.jyp.product.stock.facade;
+
+import com.jyp.product.stock.repository.LockRepository;
+import com.jyp.product.stock.service.StockService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class NamedLockStockFacade {
+
+    private LockRepository lockRepository;
+
+    private StockService stockService;
+
+    public NamedLockStockFacade(LockRepository lockRepository, StockService stockService) {
+        this.lockRepository = lockRepository;
+        this.stockService = stockService;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        try {
+            lockRepository.getLock(id.toString());
+            stockService.decrease(id, quantity);
+        } finally {
+            lockRepository.releaseLock(id.toString());
+        }
+    }
+}
